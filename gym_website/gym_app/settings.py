66322d40aa5ace3b7c44f7dc29ffbb430866acbe
Gokul Raj <serajgokul19@gmail.com>
2024-09-07 13:52:01 +0530

    Update settings.py

--- gym_website/gym_app/settings.py
+++ gym_website/gym_app/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'gym_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR /'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
