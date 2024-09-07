c6c2b6225eba23face873eb5bd0eec74fc5f5559
Gokul Raj <serajgokul19@gmail.com>
2024-09-07 22:19:36 +0530

    Update urls.py

--- gym_app/urls.py
+++ gym_app/urls.py
@@ -23,7 +23,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
